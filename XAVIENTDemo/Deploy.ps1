324fc16e7473fbd9cdebd0fe490cf3dddc3a379d
Sajid PK <sajidpk@gmail.com>
2018-05-27 21:57:26 -0400

    Tweak in deploy.ps1

--- XAVIENTDemo/Deploy.ps1
+++ XAVIENTDemo/Deploy.ps1
@@ -2,8 +2,10 @@
 # set AWS keys
 
 aws configure set aws_access_key_id $OctopusParameters["AWS Account.AccessKey"]  --profile Prod
-aws configure set aws_access_key_id $OctopusParameters["AWS Account.SecretKey"]   --profile Prod
+aws configure set aws_secret_access_key $OctopusParameters["AWS Account.SecretKey"]   --profile Prod
 aws configure set default.region us-west-2  --profile Prod
 
-
 dotnet lambda package-ci --Profile Prod   -sb  serverlessstore -ot serverless.json -cfg  aws-lambda-tools-defaults.json -pcfg True
+
+
+aws cloudformation deploy --template test.json --stack-name serverlessdemo  --parameter-overrides BucketName=serverlessstore ShouldCreateBucket=true --capabilities CAPABILITY_IAM
