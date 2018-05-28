92897f0a7124ccf1d52debff185d75d6d22dd8db
Sajid PK <sajidpk@gmail.com>
2018-05-27 21:43:10 -0400

    octopusdeploy test 2

--- XAVIENTDemo/Deploy.ps1
+++ XAVIENTDemo/Deploy.ps1
@@ -1,4 +1,9 @@
 ﻿
-aws configure set aws_access_key_id default_access_key
+# set AWS keys
+
+aws configure set aws_access_key_id $OctopusParameters["AWS Account.AccessKey"]  --profile Prod
+aws configure set aws_access_key_id $OctopusParameters["AWS Account.SecretKey"]   --profile Prod
+aws configure set default.region us-west-2  --profile Prod
+
 
 dotnet lambda package-ci --Profile Prod   -sb  serverlessstore -ot serverless.json -cfg  aws-lambda-tools-defaults.json -pcfg True
