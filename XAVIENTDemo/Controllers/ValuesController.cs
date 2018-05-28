210025216a5531cb6a035cbeb64fd5add0bca1b8
Sajid PK <sajidpk@gmail.com>
2018-05-28 03:29:43 -0400

    check post methode

--- XAVIENTDemo/Controllers/ValuesController.cs
+++ XAVIENTDemo/Controllers/ValuesController.cs
@@ -25,8 +25,10 @@ namespace XAVIENTDemo.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public string Post([FromBody]string value)
         {
+
+            return value;
         }
 
         // PUT api/values/5
