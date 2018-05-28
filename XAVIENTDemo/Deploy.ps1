4d63cf6a3cba1470c139957f2ca559c701939036
Sajid PK <sajidpk@gmail.com>
2018-05-27 21:03:43 -0400

    Ok ocupus configration

--- XAVIENTDemo/Deploy.ps1
+++ XAVIENTDemo/Deploy.ps1
@@ -1,1 +1,4 @@
-﻿dotnet lambda package-ci --Profile Prod   -sb  serverlessstore -ot serverless.json -cfg  aws-lambda-tools-defaults.json -pcfg True
+﻿
+aws configure set aws_access_key_id default_access_key
+
+dotnet lambda package-ci --Profile Prod   -sb  serverlessstore -ot serverless.json -cfg  aws-lambda-tools-defaults.json -pcfg True
